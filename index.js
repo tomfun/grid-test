27ced4f14847059dcaad8b837ffd45a4ddffb8fa
tomfun <tomfun1990@gmail.com>
2015-09-03 17:29:31 +0300

    css3

--- index.js
+++ index.js
@@ -2,34 +2,48 @@ $(document).ready(
     function () {
         var grid = $('.grid').packery({
             itemSelector: '.grid-item',
-            columnWidth: 200,
-            rowHeight: 200,
-            gutter: 0,
+            columnWidth:  200,
+            rowHeight:    200,
+            gutter:       0,
             isHorizontal: true
         });
         var onAdded = function (elem) {
-            grid.packery( 'appended', elem )//prepended support; remove too
+            grid.packery('appended', elem)//prepended support; remove too
                 // layout
                 .packery();
         };
-        var count = 6;
-        $('.add').click(function(ev) {
-            ev.preventDefault();
-            var trueFalse = function() {
-                return Math.random() > .5;
-                },
-                choices = ['grid-item--width2', 'grid-item--height2'],
-                classes = 'grid-item';
-            if (trueFalse()) {
-                classes += ' ' + choices[1];
-            }
-            if (trueFalse()) {
-                classes += ' ' + choices[0];
-            }
-            var e = $('<div/>').text(++count).addClass(classes);
-            grid.append(e);
-            onAdded(e);
-        });
+        var count = 0,
+            addNewItem = function (ev) {
+                //ev.preventDefault();
+                var trueFalse = function () {
+                        return Math.random() > .5;
+                    },
+                    img = function() {
+                        return imgs[count % imgs.length];
+                    },
+                    choices   = ['grid-item--width2', 'grid-item--height2'],
+                    classes   = 'grid-item';
+                //if (trueFalse()) {
+                //    classes += ' ' + choices[1];
+                //}
+                if (trueFalse()) {
+                    classes += ' ' + choices[0];
+                }
+                var t = $('<div class="inner"/>').text(++count).css('background', 'url(' + img() + ')').css('height', '100%'),
+                    e = $('<div/>').addClass(classes).append(t);
+                grid.append(e);
+                onAdded(e);
+            },
+            addNewItems = function(n) {
+                if (!n) {
+                    n = 10;
+                }
+                for(var faker = 0; faker < 10; faker++) {
+                    addNewItem();
+                }
+            };
+        $('.add').click(addNewItem);
+        $('.addn').click(addNewItems);
     }
 );
 
