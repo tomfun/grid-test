2a34b2afe3786ec47a178d64ab6fbb3a6a9d6c1e
tomfun <tomfun1990@gmail.com>
2015-09-21 22:35:28 +0300

    custom scroll, gyroscope

--- movementGyro.js
+++ movementGyro.js
@@ -2,13 +2,36 @@
 $(document).ready(function ($) {
     //if (gyro.hasFeature('deviceorientation')) {//машина поддерживает детект положение (повороты)
     //    $('span.debugg').text("deviceorientation is supported");
-    var accCount = 0,
-        accX     = 0,
-        accY     = 0,
-        freezing = 0.97,
-        lastX    = 0,
-        lastY    = 0;
-    gyro.frequency = 150;
+    var accCount          = 0,
+        accX              = 0,
+        accY              = 0,
+        freezing          = 0.90,
+        lastX             = 0,
+        lastY             = 0,
+        gravityThreshold  = 30,
+        gravityMultiplier = 0.5,
+        multiplier        = 20,
+        frequency         = 200,
+        freezingShleyf    = 1;
+    var shleyfIndex = 0,
+        shleyfX,
+        shleyfY,
+        shleyf      = function () {
+            $('span.debugg21').text((shleyfX / freezingShleyf).toFixed(2)).css('color', '');
+            $('span.debugg22').text((shleyfY / freezingShleyf).toFixed(2)).css('color', '');
+            window.scrollHack(shleyfX / freezingShleyf, shleyfY / freezingShleyf);
+            if (shleyfIndex++ < freezingShleyf) {
+                setTimeout(shleyf, frequency / (freezingShleyf + 1));
+            } else {
+                shleyfIndex = 0;
+                shleyfX = 0;
+                shleyfY = 0;
+                //$('span.debugg').text(
+                //    "~~@#$%~~" + Math.random().toFixed(2)
+                //);
+            }
+        };
+    gyro.frequency = frequency;
     gyro.startTracking(function (eventData) {
         // call our orientation event handler
         if (eventData.gamma === null) {
@@ -37,24 +60,32 @@ $(document).ready(function ($) {
         if (accCount === 1) {
             gyro.calibrate();
         } else {
-            $('span.debugg').text(
-                "DeviceOrientation: "
-                + ' (' + x.toFixed(1) + ',  ' + y.toFixed(1) + ')'
-            );
-            $('span.debugg21').text(accX.toFixed(2)).css('color', accX > 0 ? 'red' : 'blue');
-            $('span.debugg22').text(accY.toFixed(2)).css('color', accY > 0 ? 'red' : 'blue');
+            //$('span.debugg').text(
+            //    "DeviceOrientation: "
+            //    + ' (' + x.toFixed(1) + ',  ' + y.toFixed(1) + ')'
+            //);
+            //$('span.debugg21').text(accX.toFixed(2)).css('color', accX > 0 ? 'red' : 'blue');
+            //$('span.debugg22').text(accY.toFixed(2)).css('color', accY > 0 ? 'red' : 'blue');
+            if (!window.scrollHack) {
+                return;//hack
+            }
             var diffX = lastX - accX,
                 diffY = lastY - accY;
-            if (accX > 30) {
-                diffX -= (accX - 30) / 2;
+            if (accX > gravityThreshold) {
+                diffX -= (accX - gravityThreshold) * gravityMultiplier;
             }
-            if (accX < -30) {
-                diffX -= (accX + 30) / 2;
+            if (accX < -gravityThreshold) {
+                diffX -= (accX + gravityThreshold) * gravityMultiplier;
             }
-            if (!window.scrollHack) {
-                return;
+            if (accY > gravityThreshold) {
+                diffY -= (accY - gravityThreshold) * gravityMultiplier;
             }
-            window.scrollHack(diffX * 10, diffY * 10);
+            if (accY < -gravityThreshold) {
+                diffY -= (accY + gravityThreshold) * gravityMultiplier;
+            }
+            shleyfX = diffX * multiplier;
+            shleyfY = diffY * multiplier;
+            shleyf();
             lastX = accX;
             lastY = accY;
         }
